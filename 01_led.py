045dfe40a94b5a66a27719746bd9b91b8fc197c4
SunnyGotSkillz <33010985+SunnyGotSkillz@users.noreply.github.com>
2017-10-22 16:52:49 -0700

    Add files via upload

---
+++ 01_led.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+import RPi.GPIO as GPIO
+import time
+
+LedPin = 11    # pin11
+
+def setup():
+	GPIO.setmode(GPIO.BOARD)       # Numbers GPIOs by physical location
+	GPIO.setup(LedPin, GPIO.OUT)   # Set LedPin's mode is output
+	GPIO.output(LedPin, GPIO.HIGH) # Set LedPin high(+3.3V) to off led
+
+def loop():
+	while True:
+		print 'LED on'
+		GPIO.output(LedPin, GPIO.LOW)  # led on
+		time.sleep(1)
+		print 'LED off'
+		GPIO.output(LedPin, GPIO.HIGH) # led off
+		time.sleep(1)
+
+def destroy():
+	GPIO.output(LedPin, GPIO.HIGH)     # led off
+	GPIO.cleanup()                     # Release resource
+
+if __name__ == '__main__':     # Program start from here
+	setup()
+	try:
+		loop()
+	except KeyboardInterrupt:  # When 'Ctrl+C' is pressed, the child program destroy() will be  executed.
+		destroy()
+
